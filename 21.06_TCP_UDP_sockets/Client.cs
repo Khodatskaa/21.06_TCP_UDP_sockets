40ad3e822a7bc0f2184b35af9a0f29bd5387624c
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-07-02 12:57:57 +0300

    Ex 1 was done

---
+++ 21.06_TCP_UDP_sockets/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _21._06_TCP_UDP_sockets
+{
+    public class ExchangeRateClient
+    {
+        private static void Main()
+        {
+            TcpClient client = new TcpClient("localhost", 5000);
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                Console.Write("Enter currency pair (e.g., USD_EURO) or 'exit' to quit: ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                byte[] request = Encoding.UTF8.GetBytes(input);
+                stream.Write(request, 0, request.Length);
+
+                byte[] responseBuffer = new byte[1024];
+                int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+                string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+
+                Console.WriteLine($"Exchange Rate: {response}");
+            }
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
--- 21.06_TCP_UDP_sockets/Program.cs
+++ 21.06_TCP_UDP_sockets/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace _21._06_TCP_UDP_sockets
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace _21._06_TCP_UDP_sockets
 {
-    public class Program
+    public class ExchangeRateServer
     {
-        static void Main(string[] args)
+        private static readonly Dictionary<string, double> exchangeRates = new Dictionary<string, double>
         {
+            { "USD_EURO", 0.85 },
+            { "EURO_USD", 1.18 },
+        };
+
+        private static readonly List<string> log = new List<string>();
+
+        public static void Main()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                ThreadPool.QueueUserWorkItem(HandleClient, client);
+            }
+        }
+
+        private static void HandleClient(object clientObject)
+        {
+            TcpClient client = (TcpClient)clientObject;
+            IPEndPoint clientEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+            string clientInfo = $"{clientEndPoint.Address}:{clientEndPoint.Port}";
+
+            log.Add($"Connected: {clientInfo} at {DateTime.Now}");
+
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string request = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+                string response = GetExchangeRateResponse(request);
+                byte[] responseData = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseData, 0, responseData.Length);
+            }
+
+            log.Add($"Disconnected: {clientInfo} at {DateTime.Now}");
+            client.Close();
+        }
+
+        private static string GetExchangeRateResponse(string request)
+        {
+            if (exchangeRates.TryGetValue(request, out double rate))
+            {
+                log.Add($"Exchange rate requested: {request} at {DateTime.Now} - Rate: {rate}");
+                return rate.ToString();
+            }
+            else
+            {
+                return "Invalid currency pair";
+            }
         }
     }
 }
